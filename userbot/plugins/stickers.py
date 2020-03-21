af013471c5ea46c9cbc498186671d3e662277273
Pahul124 <60101949+Pahul124@users.noreply.github.com>
2020-03-21 13:27:54 +0530

    Update stickers.py

--- userbot/plugins/stickers.py
+++ userbot/plugins/stickers.py
@@ -46,7 +46,7 @@ async def _(event):
 
     me = borg.me
     userid = event.from_id
-    packname = f"@{user.username}'s kang pack Vol.{pack}" {userid}"
+    packname = f"@its_Pahul ki Gufa me_{userid}"
     packshortname = f"Pahul_ki_gufa_me_{userid}"  # format: Uni_Borg_userid
 
     is_a_s = is_it_animated_sticker(reply_message)
